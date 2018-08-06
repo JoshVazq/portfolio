a472f7654278abdbf060ee2b3c993f77e9572ea5
Unknown <josh.vazq@outlook.com>
2018-08-06 21:32:30 +0200

    feature(education-description): add description

--- src/model/education.js
+++ src/model/education.js
@@ -4,12 +4,14 @@ import { Period } from './period';
 export class Education extends ContentfulModel {
   school: string;
   degree: string;
+  description: string;
   dates: Period;
 
   static fromContentful(data: ContentfulEntry): Education {
     const education = new Education(data.sys.id);
     education.school = data.fields.school;
     education.degree = data.fields.degree;
+    education.description = data.fields.description;
     education.dates = new Period(new Date(data.fields.from), new Date(data.fields.to));
     return education;
   }
